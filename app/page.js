6e7fd7de0fd2d05b6b97844f0a57a4619a22a8ea
JLaxon <jlaxon@my.centennialcollege.ca>
2025-03-05 16:43:14 -0500

    added Navbar

--- app/page.js
+++ app/page.js
@@ -1,9 +1,11 @@
 import Image from "next/image";
 import HeroSection from "./components/HeroSection";
+import Navbar from "./components/NavBar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flext-col bg-[#121212]">
+      <Navbar />
       <container class="container mx-auto px-12 py-4">
         <HeroSection />
       </container>
