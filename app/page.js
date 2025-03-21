d73b9f25fa6370ab88590f2d49727506aad9a475
JLaxon <jlaxon@my.centennialcollege.ca>
2025-03-21 14:14:22 -0400

    added email and footer section

--- app/page.js
+++ app/page.js
@@ -3,16 +3,20 @@ import HeroSection from "./components/HeroSection";
 import Navbar from "./components/NavBar";
 import AboutSection from "./components/AboutSection";
 import ProjectsSection from "./components/ProjectsSection";
+import EmailSection from "./components/EmailSection";
+import Footer from "./components/Footer";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flext-col bg-[#121212]">
       <Navbar />
-      <container class="container mx-auto px-12 py-4">
+      <div class="container mx-auto px-12 py-4">
         <HeroSection />
         <AboutSection />
         <ProjectsSection />
-      </container>
+        <EmailSection />
+      </div>
+      <Footer />
     </main>
   );
 }
