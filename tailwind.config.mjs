513bed2eb998ff8149337d7c5fc00d84ce27f796
JLaxon <jlaxon@my.centennialcollege.ca>
2025-06-19 15:27:15 -0400

    Removed unnecessary lines

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,23 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 const colors = require("tailwindcss/colors");
-
-export default {
+module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic":
+          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
     colors: {
       ...colors,
-      primary: colors.green,
-      secondary: colors.blue,
+      primary: colors.purple,
+      secondary: colors.pink,
     },
   },
   plugins: [],
